e9e9b006d4696d969b32af338c1624f8f6c758fa
Bryn van Dam <Bryn.vanDam@nice.com>
2020-10-19 19:46:52 +0200

    Initial commit

---
+++ app/src/main/java/com/example/madlevel3task2/PortalsFragment.kt
@@ -0,0 +1,68 @@
+package com.example.madlevel3task2
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResultListener
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.fragment_portals.*
+
+/**
+ * A simple [Fragment] subclass as the default destination in the navigation.
+ */
+class PortalsFragment : Fragment() {
+    private val portals = arrayListOf<Portal>()
+    private val portalAdapter = PortalAdapter(portals)
+
+    override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_portals, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        initViews()
+        observeAddReminderResult()
+    }
+
+    fun initViews() {
+        // Initialize the recycler view with a linear layout manager, adapter
+        rvPortals.layoutManager =
+            LinearLayoutManager(context, RecyclerView.VERTICAL, false)
+        rvPortals.adapter = portalAdapter
+        rvPortals.addItemDecoration(
+            DividerItemDecoration(
+                context,
+                DividerItemDecoration.VERTICAL
+            )
+        )
+//        createItemTouchHelper().attachToRecyclerView(rvPortals)
+    }
+
+    private fun observeAddReminderResult() {
+        setFragmentResultListener(REQ_REMINDER_KEY) { key, bundle ->
+            bundle.getString(BUNDLE_REMINDER_KEY)?.let {
+                val portal = Portal(it, it)
+
+                portals.add(portal)
+                portalAdapter.notifyDataSetChanged()
+            } ?: Log.e("ReminderFragment", "Request triggered, but empty reminder text!")
+
+        }
+    }
+
+//    private fun createItemTouchHelper(): ItemTouchHelper {
+//        return ItemTouchHelper(null)
+//    }
+}
